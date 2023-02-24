61b0e04c5fb3777ea6bd7ceb080e55fe9dadd64b
mcfanda <mcfanda@gmail.com>
2023-02-24 15:06:24 +0100

    version 3

--- R/myregression.b.R
+++ R/myregression.b.R
@@ -5,75 +5,39 @@ myRegressionClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
         .runner=NULL,
         # this is a list that contains all the SmartTables
         .tables=list(),
-        .time=NULL,
         .init = function() {
-            jinfo("MODULE:  #### init phase start ####")
-            private$.time<-Sys.time()
             
             if (!is.something(self$options$dep) | !is.something(self$options$covs))
                 return()
-            # define the table
+            # define the runner
             private$.runner<-Runner$new(self)
             
             # set up the coefficients SmartTable
-            atable<-SmartTable$new(self$results$main$coefficients,private$.runner)
+            atable<-SmartTable$new(self$results$coefficients,private$.runner)
             # put the SmartTable in the list
             private$.tables[[length(private$.tables)+1]]<-atable
-            
             # set up the anova SmartTable
-            atable<-SmartTable$new(self$results$main$anova,private$.runner)
-            atable$hideOn <- list(df1=Inf)
+            atable<-SmartTable$new(self$results$anova,private$.runner)
             # put the SmartTable in the list
             private$.tables[[length(private$.tables)+1]]<-atable
-            
-            # set up the means SmartArray
-            obj<-SmartArray$new(self$results$means,private$.runner)
-            # put the SmartArray in the list
-            private$.tables[[length(private$.tables)+1]]<-obj
-
-            lapply(private$.tables,function(x) x$initTable())
-            jinfo("MODULE:  #### init phase end ####")
-            
-            # set up the means SmartArray
-            obj<-SmartArray$new(self$results$sig$means,private$.runner)
-            # put the SmartArray in the list
-            private$.tables[[length(private$.tables)+1]]<-obj
-
-            
-            
             # set up the effects SmartTable
-            atable<-SmartTable$new(self$results$additional$effects,private$.runner)
-            atable$spaceBy <- "var"
-            atable$superTitle <- list(es_ci_lower="95% confidence interval",es_ci_upper="95% confidence interval")
+            atable<-SmartTable$new(self$results$effects,private$.runner)
             # put the SmartTable in the list
             private$.tables[[length(private$.tables)+1]]<-atable
-
-
-            # set up the means SmartTable
-            obj<-SmartTable$new(self$results$correlations,private$.runner)
-            obj$expandOnInit  <- TRUE
-            obj$expandFrom    <- 2
-            private$.tables[[length(private$.tables)+1]]<-obj
             
             # init all tables
+            lapply(private$.tables,function(x) x$initTable())
             
         },
         .run = function() {
             
             if (!is.something(self$options$dep) | !is.something(self$options$covs))
                 return()
-            jinfo("MODULE:  #### run phase start ####")
-            
             # estimate the model             
             private$.runner$estimate()
             # execute all SmartTable run functions             
             lapply(private$.tables,function(x) x$runTable())
             
-            jinfo("MODULE:  #### run phase end ####")
-            now<-Sys.time()
-            jinfo("TIME:",now-private$.time," secs")
-            
         }
     )
 )
-
